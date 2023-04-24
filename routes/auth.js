4d5c3041b827805ffddb6b60153ebe72f4e0b644
Josey Sekine <ipho.7johero@icloud.com>
2023-04-24 12:52:54 -0700

    Backend delete and edit

--- routes/auth.js
+++ routes/auth.js
@@ -7,7 +7,8 @@ router.post('/signin', authController.postSignIn);
 router.get('/login', authController.getLogin);
 router.post('/login', authController.postLogin);
 router.get('/user/edit/:userId', authController.getEditUser);
-router.post('/edit-user', authController.postEditUser);
+router.post('/user/edit/:userId', authController.postEditUser);
+router.delete('/user/delete/:userId', authController.deleteUser)
 router.get('/user', authController.userInfo);
 
 module.exports = router;
